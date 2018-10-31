f3f6219a9e3673b17bfe9c36a30f05a352a16b4d
iruy0489 <iruy_ingegneri0825@yahoo.co.jp>
2018-10-31 23:11:05 +0900

    作成

---
+++ Reversi/Reversi/test.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <windows.h>
+#include "DxLib.h"
+
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int){
+	DxLib_Init();   // DXライブラリ初期化処理
+
+
+	WaitKey();      // キー入力待ち
+	DxLib_End();    // DXライブラリ終了処理
+	return 0;
+}
