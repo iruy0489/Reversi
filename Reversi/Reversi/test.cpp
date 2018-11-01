74fbbd305d59f5445e26e32037ad722012bb4551
iruy0489 <iruy_ingegneri0825@yahoo.co.jp>
2018-11-01 19:53:38 +0900

    マップ、列挙型更新

--- Reversi/Reversi/test.cpp
+++ Reversi/Reversi/test.cpp
@@ -2,6 +2,45 @@
 #include <windows.h>
 #include "DxLib.h"
 
+
+enum BordState{
+	NONE = -1,
+	BLACK = 0,
+	WHITE = 1,
+	BLANK = 2
+};
+
+enum Player{
+	ONE = 0,
+	TWO = 1
+};
+
+enum ChipSurround{
+	UPPER_LEFT = 0,
+	UPPER_MIDDLE = 1,
+	UPPER_RIGHT = 2,
+	LEFT = 3,
+	RIGHT = 4,
+	DOWN_LEFT = 5,
+	DOWN_MIDDLE = 6,
+	DOWN_RIGHT = 7
+};
+
+struct Bord{
+	int *xBordSize;
+	int *yBordSize;
+	char image[10];
+	BordState bordStatu;
+};
+
+int FieldBord(Bord bord[9][9], int &oneBordSize, char image[10]){
+	for(int i = 0; i < 9; i++){
+		for(int k = 0; k < 9; k++){
+			bord[i][k] = {&oneBordSize, &oneBordSize, image[10]}, BLANK;
+		}
+	}
+}
+
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int){
 	DxLib_Init();   // DXƒ‰ƒCƒuƒ‰ƒŠ‰Šú‰»ˆ—
 
